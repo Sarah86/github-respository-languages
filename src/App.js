983d7217c8bc0689d97031801a74b68930295a6b
Sarah86 <sarah@DESKTOP-VI58DL9.localdomain>
2022-05-19 01:06:14 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const groupByLanguage = ({ languages, results }) => {
+  let groupedByLanguage = {};
+  languages.forEach((language) => {
+    const filteredByLanguage = results.filter(
+      (item) => item.language === language
+    );
+    groupedByLanguage = {
+      ...groupedByLanguage,
+      [language]: filteredByLanguage,
+    };
+  });
+  return groupedByLanguage;
+};
+
+const getLanguages = ({ results }) => [
+  ...new Set(results.map((item) => item.language)),
+];
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [totalCount, setTotalCount] = useState(0);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (query) {
+      setLoading(true);
+      fetch(
+        `https://api.github.com/search/repositories?q="${query}"&per_page=1000`
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((json) => {
+          setTotalCount(json.total_count);
+          if (json.items) {
+            const filteredResult = json.items.filter((item) => item.language);
+            const languages = getLanguages({ results: filteredResult });
+            const groupedByLanguage = groupByLanguage({
+              languages,
+              results: filteredResult,
+            });
+            const descendingLanguage = languages.sort(
+              (a, b) =>
+                groupedByLanguage[b].length - groupedByLanguage[a].length
+            );
+            const finalResult = descendingLanguage.map((language) => ({
+              [language]: groupedByLanguage[language].length,
+            }));
+            setResult(finalResult);
+          }
+          setLoading(false);
+        })
+        .catch((error) => {
+          setLoading(false);
+          setError(error);
+        });
+    }
+  };
+  const onInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Holly Health Test</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          onChange={onInputChange}
+          placeholder="Search for a repository"
+        />
+        <input
+          className="submitButton"
+          disabled={loading}
+          type="submit"
+          value={loading ? "Loading..." : "Go"}
+        />
+      </form>
+      {error && <span>{error}</span>}
+      {result.length ? (
+        <>
+          <div className="tableContainer">
+            <table>
+              <thead>
+                <tr>
+                  <th>Language</th>
+                  <th>Count</th>
+                </tr>
+              </thead>
+              <tbody>
+                {result.map((language) => {
+                  const name = Object.keys(language)[0];
+                  const count = Object.values(language)[0];
+                  return (
+                    <tr key={name}>
+                      <td>{name}</td>
+                      <td>{count}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+          <span>{totalCount} result(s) found</span>
+        </>
+      ) : null}
     </div>
   );
 }
